d23653e797dd84c3a16417f1dd5857af3cec42f8
Mutton9558 <69904687+Mutton9558@users.noreply.github.com>
2024-08-17 22:57:25 +0800

    Fix backend issues and implemented connection to the blockchain database

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -5,12 +5,24 @@ function sendIC() {
 
         console.log(r);
         let response = JSON.parse(r);
+        let md = JSON.parse(r).result.metadata
+        md = JSON.parse(md)
         console.log(response.result == null);
 
         if (response.result == null) {
             document.querySelector("#wrongID").innerHTML = 'Medical ID does not exist!';
         } else {
-            setLocalStorage('medical_id', id);
+            localStorage.setItem('medical_id', id);
+            localStorage.setItem('name', md['name'])
+            localStorage.setItem('age', md['age'])
+            localStorage.setItem('gender', md['gender'])
+            localStorage.setItem('nationality', md['nationality'])
+            localStorage.setItem('bloodtype', md['bloodtype'])
+            localStorage.setItem('height', md['height'])
+            localStorage.setItem('weight', md['weight'])
+            localStorage.setItem('date_list', md['date'])
+            localStorage.setItem('activity_list', md['activity'])
+            localStorage.setItem('status_list', md['status'])
             window.location.href = './user.html';
         }
     })
